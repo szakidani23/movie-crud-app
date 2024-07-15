44053d6818351e4a716beeb2ef5a24def1714319
Daniel Szakaly <129371637+szakidani23@users.noreply.github.com>
2024-07-15 14:37:06 +0200

    Movie Class, Array + markup for home (userinputs, table) added.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Movie } from '../movie';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  movies: Movie[] = [];
+  movie: Movie = new Movie();
+  movieLocalDB: string = 'movieLocalDB';
+}
--- src/app/movie.ts
+++ src/app/movie.ts
@@ -1,2 +1,20 @@
+import { Guid } from 'guid-typescript';
 export class Movie {
+  id: string = Guid.create().toString();
+  name: string = '';
+  genre: string = '';
+  year: number | null = null;
+  duration: number | null = null;
+  ageRating: number | null = null;
+  imdbScore: number | null = null;
+
+  resetProperties() {
+    this.id = Guid.create().toString();
+    this.name = '';
+    this.genre = '';
+    this.year = null;
+    this.duration = null;
+    this.ageRating = null;
+    this.imdbScore = null;
+  }
 }
