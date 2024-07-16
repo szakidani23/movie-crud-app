7cc8baa92bca506a21a28a6f004c129b29a7018a
Daniel Szakaly <129371637+szakidani23@users.noreply.github.com>
2024-07-16 08:14:54 +0200

    Test Data created for table, 2 Buttons added to the table managing the edit state

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,4 +10,38 @@ export class HomeComponent {
   movies: Movie[] = [];
   movie: Movie = new Movie();
   movieLocalDB: string = 'movieLocalDB';
+  editEnabled: boolean = false;
+
+  constructor() {
+    this.seedData();
+  }
+
+  seedData() {
+    let d = new Movie();
+    d.name = 'Test Movie1';
+    d.genre = 'Test Genre';
+    d.year = 1998;
+    d.duration = 128;
+    d.ageRating = 12;
+    d.imdbScore = 6.9;
+    this.movies.push(d);
+
+    d = new Movie();
+    d.name = 'Test Movie2';
+    d.genre = 'Test Genre';
+    d.year = 1996;
+    d.duration = 121;
+    d.ageRating = 6;
+    d.imdbScore = 8.6;
+    this.movies.push(d);
+
+    d = new Movie();
+    d.name = 'Test Movie3';
+    d.genre = 'Test Genre';
+    d.year = 2012;
+    d.duration = 145;
+    d.ageRating = 18;
+    d.imdbScore = 6.3;
+    this.movies.push(d);
+  }
 }
