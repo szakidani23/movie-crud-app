ce030495aeb82b0c86cc3108cb9af0fd1c01b641
Daniel Szakaly <129371637+szakidani23@users.noreply.github.com>
2024-07-31 07:34:57 +0200

    Started to implement movie quiz logic
    Base variables added

--- src/app/movie-quiz-game/movie-quiz-game.component.ts
+++ src/app/movie-quiz-game/movie-quiz-game.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
+import { Movie } from '../models/movie';
 
 @Component({
   selector: 'app-movie-quiz-game',
   templateUrl: './movie-quiz-game.component.html',
-  styleUrl: './movie-quiz-game.component.scss'
+  styleUrl: './movie-quiz-game.component.scss',
 })
-export class MovieQuizGameComponent {
+export class MovieQuizGameComponent implements OnInit {
+  movies: Movie[] = [];
+  currentQuestion: any[] = []; /// will think about it later
+  selectedAnswer: any[] = []; /// will think about it later
+  quizOver: boolean = false;
+  score: number = 0;
 
+  constructor(public movieService: MovieService) {}
+
+  ngOnInit(): void {
+    this.movies = this.movieService.movies;
+  }
 }
